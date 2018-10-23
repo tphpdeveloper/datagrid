1868694b0b466b2bb414b49185f12997345735d3
tphpdeveloper <igorkutsan@ukr.net>
2018-10-23 20:25:47 +0300

    Change structure

--- src/resources/view/datagrid/grid.blade.php
+++ src/resources/view/datagrid/grid.blade.php
@@ -1,60 +1,112 @@
 <div class="table-responsive">
-    <table class="table">
+    <table {!! $builder->getStringAttributes() != "" ? $builder->getStringAttributes() : 'class="table"' !!}>
         <thead class="text-primary">
 
-            {{--{{ dd()}}--}}
+        {{--{{ dd()}}--}}
+        <tr>
+            @foreach($columns as $column)
+                <th>
+                    @if($column->hasSort())
+                    @php
+                        $fa_sort = 'fa-sort';
+                        $url_except = $request->except('sort.'.$column->getName());
+                        if($request->has('sort.'.$column->getName())){
+                            $key = $request->input('sort.'.$column->getName());
+                            if($key == 'ASC'){
+                                $url_except['sort'][$column->getName()] = 'DESC';
+                                $fa_sort = 'fa-sort-asc';
+                            }
+                            else{
+                                $url_except['sort'][$column->getName()] = 'ASC';
+                                $fa_sort = 'fa-sort-desc';
+                            }
+                        }
+                        else{
+                            $url_except['sort'][$column->getName()] = 'ASC';
+                        }
+
+                        $url = route($request->route()->getName(), $url_except);
+                    @endphp
+                    <a href="{{ $url }}">
+                        {{ $column->getAlias() }}
+                        {!! Html::tag('i', '', ['class' => 'fa '.$fa_sort]) !!}
+                    </a>
+                    @else
+                        {{ $column->getAlias() }}
+                    @endif
+                </th>
+            @endforeach
+
+            @if($builder->getRow()->hasSort())
+                <th class="text-right">
+                    @php
+                        $without_sort = $request->except('sort');
+                    @endphp
+                    {!! Html::link(route($request->route()->getName(), $without_sort), Html::tag('i', '', ['class' => 'fa fa-remove']),
+                        ['class' => 'btn text-danger'],
+                        null,
+                        false) !!}
+                </th>
+            @endif
+
+        </tr>
+        @if($builder->getRow()->hasFilter())
             <tr>
-                @foreach($columns as $co => $column)
-                    @continue(($co + 1) == count($columns) && $column->getName() === \Tphpdeveloper\Gridview\Datagrid\Column::COLUMN_CALLBACK)
-                    <th>
-                        @if($column->hasSort() && $column->getName() !== \Tphpdeveloper\Gridview\Datagrid\Column::COLUMN_CALLBACK)
-                            @include('datagrid.sort.sort')
-                        @else
-                            {{ $column->getAlias() }}
-                        @endif
-                    </th>
+                {{--filter field for search  --}}
+                @foreach($columns as $column)
+                    @if($column->hasFilter())
+                        <th>
+                            {!! Form::text('filter['.$column->getName().']',
+                                ($request->input('filter.'.$column->getName()) ?? ''),
+                                [
+                                    'class' => 'form-control',
+                                    'form' => 'filter_search'
+                                ]) !!}
+                        </th>
+                    @else
+                        <th></th>
+                    @endif
                 @endforeach
+                <th class="text-right">
+                    {{--button form for search by filter data--}}
+                    {!! Form::open(['url' => $request->url(), 'method' => 'GET', 'id' => 'filter_search']) !!}
 
-                @include('datagrid.sort.button')
+                        {!! Form::button(
+                                Html::tag('i', '', ['class' => 'fa fa-search']),
+                                [
+                                'type' => 'submit',
+                                'class' => 'btn text-success'
+                                ]
+                        ) !!}
 
+                        {!! Html::link(route($request->route()->getName(), $request->except('filter')), Html::tag('i', '', ['class' => 'fa fa-remove']),
+                                [
+                                'class' => 'btn text-danger'
+                                ],
+                                null,
+                                false) !!}
+                    {!! Form::close() !!}
+
+                </th>
             </tr>
-            @include('datagrid.filter.filter')
+        @endif
         </thead>
         <tbody>
-            @if($models)
-                @php
-                    $page = ($request->page ?? 0);
-                    $count_on_first_page = 0;
-                    if($models instanceof \Illuminate\Pagination\LengthAwarePaginator){
-                        $total_page = $models->lastPage();
-                        $total_product = $models->total();
-                        $count_on_first_page = ceil($total_product / $total_page);
-                    }
-                //dd($columns);
-                @endphp
-                @foreach($models as $co => $model)
-                    <tr>
-                        @foreach($columns as $co_column => $column)
-                            <td {!! $column->getStringAttributes() !!} >
-                                @if($column->getName() === \Tphpdeveloper\Gridview\Datagrid\Column::COLUMN_COUNTER)
-                                    {{ (!$page ? $co + 1 : ( $co + 1 + ( ( $page * $count_on_first_page ) - $count_on_first_page ) ) ) }}
-                                @elseif($column->getName() === \Tphpdeveloper\Gridview\Datagrid\Column::COLUMN_CALLBACK)
-                                    {!! $column->getCollback($model) !!}
-                                @else
-                                    {!! $model->{$column->getName()} !!}
-                                @endif
-                            </td>
-                        @endforeach
-                        @if(($co_column + 1) == count($columns) && $column->getName() !== \Tphpdeveloper\Gridview\Datagrid\Column::COLUMN_CALLBACK)
-                            <td></td>
-                        @endif
-                    </tr>
-                @endforeach
-            @endif
+        {{--<tr>--}}
+        {{--<td>--}}
+        {{--Dakota Rice--}}
+        {{--</td>--}}
+        {{--<td>--}}
+        {{--Niger--}}
+        {{--</td>--}}
+        {{--<td>--}}
+        {{--Oud-Turnhout--}}
+        {{--</td>--}}
+        {{--<td class="text-right">--}}
+        {{--$36,738--}}
+        {{--</td>--}}
+        {{--</tr>--}}
 
         </tbody>
     </table>
-    @if($models instanceof \Illuminate\Pagination\LengthAwarePaginator)
-        {!! $models->links('vendor.pagination.bootstrap-4') !!}
-    @endif
 </div>
